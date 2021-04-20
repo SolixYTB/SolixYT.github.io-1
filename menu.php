9add963a805e66308b5843fbfd09ba23483925b9
SolixYT <82849804+SolixYT@users.noreply.github.com>
2021-04-19 20:11:06 -0400

    Delete menu.php

--- menu.php
+++
@@ -1,8 +0,0 @@
-            <nav>
-                <ul>
-                    <li ip="logo"><a href="#">Solix</a></li>
-                    <li><a href="exo.php">exo2</a></li>
-                    <li><a href="test.php">test</a></li>
-                    <li><a href="test2.php">test2</a></li>
-                </ul>
-            </nav>
